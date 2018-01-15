14e0787680b24822154682df81aa778e151238fe
Rachel Moore <rachel@rachelmoore.net>
2018-01-15 00:00:16 -0800

    Change onSubmit for goToStore method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
     goToStore(event) {
         event.preventDefault(); //stop default from happening (refresh the page)
         // first grab text from box
@@ -12,7 +17,7 @@ class StorePicker extends React.Component {
 
     render() {
         return (
-            <form className="store-selector" onSubmit={this.goToStore} >
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)} >
                 <h2>Please enter a store</h2>
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/> 
                 <button type="submit"> Visit Store</button>
