82030331a2cd8fc0e6cd79b1556cb1914d3127dc
Rachel Moore <rachel@rachelmoore.net>
2018-01-14 22:33:08 -0800

    Import react router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
