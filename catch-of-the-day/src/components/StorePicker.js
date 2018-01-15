ef2e914d6de5dd0454bf8b51a1b06d3dbe731998
Rachel Moore <rachel@rachelmoore.net>
2018-01-15 00:02:37 -0800

    Test storeInput value in goToStore method

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -10,14 +10,14 @@ class StorePicker extends React.Component {
     goToStore(event) {
         event.preventDefault(); //stop default from happening (refresh the page)
         // first grab text from box
-        console.log(this.storeInput);
+        console.log(this.storeInput.value);
         // second, transition to / to /store/:storeId
         console.log("you changed the url");
     }
 
     render() {
         return (
-            <form className="store-selector" onSubmit={(e) => this.goToStore(e)} >
+            <form className="store-selector" onSubmit={this.goToStore} >
                 <h2>Please enter a store</h2>
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/> 
                 <button type="submit"> Visit Store</button>
