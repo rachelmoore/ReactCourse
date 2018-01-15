4871841b0d2bddffbbc780b01d2691adad8564d1
Rachel Moore <rachel@rachelmoore.net>
2018-01-15 15:56:34 -0800

    Create Add Fish Form

---
+++ catch-of-the-day/src/components/addFishForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    render() {
+        return (
+            <h2>Add Fish Form</h2>
+        )
+    }
+}
+
+export default AddFishForm;
