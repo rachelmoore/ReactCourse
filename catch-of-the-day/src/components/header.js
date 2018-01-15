3dd0d58ee0d24b36ff324d1303cc05cb0209ae4b
Rachel Moore <rachel@rachelmoore.net>
2018-01-14 19:10:27 -0800

    Style catch of the day banner'

--- catch-of-the-day/src/components/header.js
+++ catch-of-the-day/src/components/header.js
@@ -4,8 +4,15 @@ class Header extends React.Component {
     render () {
         return (
             <header className="top">
-                <h1>Catch of the Day</h1>
-                <h3 className="tagline">Fill Me In</h3>
+                <h1>
+                    Catch 
+                    <span className="ofThe">
+                    <span className="of">of</span> 
+                    <span className="the">the</span> 
+                    </span>
+                    Day
+                </h1>
+                <h3 className="tagline">{this.props.tagline}</h3>
             </header>
         )
     }
