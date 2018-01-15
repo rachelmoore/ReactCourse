192cbca9671181adbd31e0d143dbd607fe97265a
Rachel Moore <rachel@rachelmoore.net>
2018-01-14 23:43:07 -0800

    Generate random store name

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault(); //stop default from happening (refresh the page)
+        // first grab text from box
+
+        // second, transition to / to /store/:storeId
+        console.log("you changed the url");
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore} >
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store Name" /> 
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/> 
                 <button type="submit"> Visit Store</button>
             </form>
         ) 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -3,6 +3,19 @@ import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
+import notFound from './components/notFound';
 import StorePicker from './components/StorePicker';
 
-render(<App />, document.querySelector('#main'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+        <div>
+            <Match exactly pattern="/" component={StorePicker} />
+            <Match exactly pattern="/store/:storeId" component={App} />
+            <Miss component={notFound} /> 
+        </div>
+        </BrowserRouter>
+    )
+}
+
+render(<Root />, document.querySelector('#main'));
