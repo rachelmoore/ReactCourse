499017c1b4b323ce1b14761caf91023edf1fd0ee
Rachel Moore <rachel@rachelmoore.net>
2018-01-15 19:01:49 -0800

    Finish createFish function

--- catch-of-the-day/src/components/addFishForm.js
+++ catch-of-the-day/src/components/addFishForm.js
@@ -16,12 +16,13 @@ class AddFishForm extends React.Component {
             desc: this.desc.value,
             image: this.image.value
         }
-        console.log(fish);
+        this.props.addFish(fish);
+        this.fishForm.reset();
     }
 
     render() {
         return (
-            <form className="fish-edit" onSubmit={this.createFish}>
+            <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={this.createFish}>
                 <input ref={(input) => this.name = input} type="text" placeholder="Fish Name" /> 
                 <input ref={(input) => this.price = input} type="text" placeholder="Fish Price" /> 
                 <select ref={(input) => this.status = input}>
