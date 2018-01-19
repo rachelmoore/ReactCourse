8dc8a66cdcfd12e0318799de2e1d277768c5b4e6
Rachel Moore <rachel@rachelmoore.net>
2018-01-18 17:59:29 -0800

    Refactor props const

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 class Fish extends React.Component {
     render() {
+        const details = this.props.details;
         return (
         <li className="menu-fish">
-            <img src={this.props.details.image} alt={this.props.details.name} /> 
-            {this.props.details.name}
+            <img src={details.image} alt={details.name} /> 
+            {details.name}
         </li>
         )
     }
