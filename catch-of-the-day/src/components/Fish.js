444fd972f6ca4c0cb30fc223ca60bd780ecbe437
Rachel Moore <rachel@rachelmoore.net>
2018-01-18 17:58:13 -0800

    Fish image tag uses name

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,7 +4,7 @@ class Fish extends React.Component {
     render() {
         return (
         <li className="menu-fish">
-            <img src={this.props.details.image} /> 
+            <img src={this.props.details.image} alt={this.props.details.name} /> 
             {this.props.details.name}
         </li>
         )
