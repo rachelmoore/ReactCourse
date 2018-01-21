1bd175384b6fa64b8aa3856cb89d15b34e0dbf84
Rachel Moore <rachel@rachelmoore.net>
2018-01-21 14:17:33 -0800

    Disable button display if 0 available in state

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -5,7 +5,7 @@ class Fish extends React.Component {
     render() {
         const details = this.props.details;
         const isAvailable = details.status === 'available';
-        const buttonText = isAvailable? "Add To Order" : "Sold Out!";
+        const buttonText = isAvailable ? "Add To Order" : "Sold Out!";
         return (
         <li className="menu-fish">
             <img src={details.image} alt={details.name} /> 
@@ -14,7 +14,7 @@ class Fish extends React.Component {
                 <span className="price">{formatPrice(details.price)}</span>
             </h3>
             <p>{details.desc}</p>
-            <button>{buttonText}</button>
+            <button disabled={!isAvailable}>{buttonText}</button>
         </li>
         )
     }
