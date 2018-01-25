4f6e0d8add528ac37e61d6407c961838f15dbed6
Rachel Moore <rachel@rachelmoore.net>
2018-01-24 22:15:53 -0800

    orderIds var

--- catch-of-the-day/src/components/order.js
+++ catch-of-the-day/src/components/order.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 class Order extends React.Component {
     render() {
+      const orderIds = Object.keys(this.props.order);
+      const total = orderIds.reduce()
         return (
             <h2>Your Order</h2>
         )
