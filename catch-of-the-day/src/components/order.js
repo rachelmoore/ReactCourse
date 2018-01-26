3a7a9726b54fed67e0a49de0dca0ed176c7aca75
Rachel Moore <rachel@rachelmoore.net>
2018-01-25 21:41:56 -0800

    return fish nameprice in renderOrder function

--- catch-of-the-day/src/components/order.js
+++ catch-of-the-day/src/components/order.js
@@ -13,6 +13,7 @@ class Order extends React.Component {
         return )
           <li key={key}>
             <span>{count}lbs {fish.name}</span>
+            <span className="price">{formatPrice(count * fish.price)}</span>
           </li>
         )
     }
