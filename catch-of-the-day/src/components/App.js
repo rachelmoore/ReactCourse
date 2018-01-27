81f745e6a0a4e40ea01911ffdc53c4018e831704
Rachel Moore <rachel@rachelmoore.net>
2018-01-26 16:55:12 -0800

    Import base to App component

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,7 @@ import Order from './order';
 import Inventory from './inventory';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
     constructor() {
