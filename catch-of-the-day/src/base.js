92d46411b9d891f1293161d938037e083fbaee16
Rachel Moore <rachel@rachelmoore.net>
2018-01-26 16:54:05 -0800

    import Rebase info

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,5 +1,7 @@
 import Rebase from 're-base';
 
 const base = Rebase.createClass({
-  
+  apiKey: "AIzaSyCKYoYsq8nHXwhdf0RzQO4qL3PNd8e-uFM",
+  authDomain: "cotd-e85b0.firebaseapp.com",
+  databaseURL: "https://cotd-e85b0.firebaseio.com",
 });
