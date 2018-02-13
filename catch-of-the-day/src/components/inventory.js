39dd03d8a2d546f749bcb3338b536b55ebe07144
Rachel Moore <rachel@rachelmoore.net>
2018-02-12 21:11:06 -0800

    Remove Fish button in Inventory

--- catch-of-the-day/src/components/inventory.js
+++ catch-of-the-day/src/components/inventory.js
@@ -35,6 +35,7 @@ class Inventory extends React.Component {
           </textarea>
           <input type="text" name="image" value={fish.image} placeholder="Fish Image"
           onChange={(e) => this.handleChange(e, key)} />
+          <button>Remove Fish</button>
         </div>
       )
     }
