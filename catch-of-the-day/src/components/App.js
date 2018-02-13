ebcf0a1fb16f198c8fb44f7088503db136d1ab71
Rachel Moore <rachel@rachelmoore.net>
2018-02-13 00:37:05 -0800

    Re set order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -88,7 +88,7 @@ class App extends React.Component {
     removeFromOrder(key) {
       const order = {...this.state.order};
       order[key] = null;
-      this.setState
+      this.setState({ order: order });
     }
 
     render() {
